50eed1e8084aae377bfd13829a03246ddadcaffb
Arthur França <eng.arthurfranca@gmail.com>
2024-02-18 13:05:26 -0300

    fix title and favicon

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+﻿﻿#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+echo 'www.arthurfranca.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:engArthurFranca/fancy-portifolio-vue.git main:gh-pages
+cd -
